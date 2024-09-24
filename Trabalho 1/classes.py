ce1f692f5ee539dd804c2eeb926e7eb8c6f4f0df
andrefm10 <andrefilipmartins@gmail.com>
2024-09-24 11:49:13 -0300

    Banco de dados e classes criados

--- Trabalho 1/classes.py
+++ Trabalho 1/classes.py
@@ -1,10 +1,35 @@
-class Carros:
-    def __init__(self, nome, cor, tipo, local, qualidades):
+
+
+class Veiculo:
+    def __init__(self, nome, placa, cor, ano, marca, tipo, valor, status):
         self.tipo = tipo
-        self.nome = nome
         self.cor = cor
-        self.local = local
-        self.qualidades = qualidades
+        self.nome = nome
+        self.placa = placa
+        self.marca = marca
+        self.ano = ano
+        self.valor = valor
+        self.status = status
     
-    def aluga_carro(self):
-        print("")
+
+class Cliente:
+    def __init__(self, nome_cliente, cpf, tel, email, cnh, historico):
+        self.cnh = cnh
+        self.nome_cliente = nome_cliente
+        self.cpf = cpf
+        self.tel = tel
+        self.email = email
+        self.historico = historico
+
+class Aluguel:
+    def __init__(self, datai, dataf, kmi, valort, sts_pagamento): 
+        self.datai = datai
+        self.dataf = dataf
+        self.kmi = kmi
+        self.valort = valort
+        self.sts_pagamento = sts_pagamento
+
+
+class SistemaAluguel:
+    def __init__(self,) -> None:
+        pass
